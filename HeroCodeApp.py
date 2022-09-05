e5f5c6a079761ff1a2797f7c946993852dda06e4
Mercurial5 <dias.nespayev@gmail.com>
2022-09-05 07:58:26 +0600

    feat: create app

---
+++ HeroCodeApp.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from os import getenv
+
+from HeroCode import create_app
+
+
+def main():
+    load_dotenv()
+
+    host = getenv('host')
+    port = getenv('port')
+    debug = getenv('debug') == '1'
+
+    if None in [host, port, debug]:
+        raise Exception('Host, port or debug values not given')
+
+    app = create_app()
+
+    app.run(host=host, port=port, debug=debug)
+
+
+if __name__ == '__main__':
+    main()
